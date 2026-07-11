a750586539c2f9024f58434b0f06bd78d016f571
Matthias Guenther <matthias@wikimatze.de>
2026-07-11 05:11:55 +0200

    Add custom ParseException

--- src/main/kotlin/com/example/csvusersync/CsvParseException.kt
+++ src/main/kotlin/com/example/csvusersync/CsvParseException.kt
@@ -1,4 +1,6 @@
 package com.example.csvusersync
 
-class CsvParseException {
-}
+class CsvParseException(
+    message: String,
+    val line: Int? = null
+) : Exception(if (line != null) "CSV parse error at line $line: $message" else "CSV parse error: $message")
--- src/main/kotlin/com/example/csvusersync/CsvParser.kt
+++ src/main/kotlin/com/example/csvusersync/CsvParser.kt
@@ -1,9 +1,16 @@
 package com.example.csvusersync
 
 class CsvParser {
-    fun parse(lines: List<String>): List<User> =
-        lines.drop(1).map { line ->
+    fun parse(lines: List<String>): List<User> {
+
+        val header = lines.first()
+        require(header == "user_id,mail") {
+            throw CsvParseException("expected header 'user_id,email' but was '$header'")
+        }
+        
+        return lines.drop(1).map { line ->
             val parts = line.split(",")
             User(parts[0], parts[1])
         }
+    }
 }
